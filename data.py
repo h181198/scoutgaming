c328724dbaeebb3f135a646bc1ea8de23a2ccc9d
h181198 <h181198@stud.hvl.no>
2019-03-07 11:08:01 +0100

    Add some default files

---
+++ data.py
@@ -0,0 +1,10 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Article',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique ex dolore molestiae cupiditate facilis iusto, deserunt eius assumenda deleniti neque minus blanditiis asperiores nihil! Dolore voluptatum voluptate molestiae architecto consectetur.'
+			'author': 'Nemo',
+			'published_at': '27-02-01 11:54:32'
+		}
+	]
