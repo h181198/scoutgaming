23bf8d274208b4b5c852978a9bdb221a99148e8e
h181198 <h181198@stud.hvl.no>
2019-03-11 13:50:32 +0100

    Update gitignore, add model department

---
+++ Models/Department.py
@@ -0,0 +1,16 @@
+from app import database
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+# This is only a test, not sure if it will be a part of the finished product like this
+class Department(Base):
+    __tablename__ = 'departments'
+
+    id = Column(Integer, primary_key=True)
+    country = Column(String)
+    unit = Column(String)
+
+    def __repr__(self):
+        return "<Department (id='%s', country='%s', unit='%s') >" % (self.id, self.country, self.unit)
