251ffaddd6d74d2959698d831fba2a4e46f5e755
h181198 <h181198@stud.hvl.no>
2019-03-11 12:40:22 +0100

    Add sqlquery and make app.py run with local database

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_admin import Admin
+from sqlalchemy import create_engine
+
 
 app = Flask(__name__)
+database = create_engine('postgres://localhost:5432/myDatabase')
+database.connect()
+
 
 @app.route('/')
-def index():
-	return render_template('index.html')
+def hello_world():
+    return 'Hello World!'
 
+
+admin = Admin(app)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+
+    app.run(debug=True)
