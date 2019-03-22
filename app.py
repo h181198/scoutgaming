596c7bf724709de51f8b198be0b5584ad80263a8
h182020 <endretbotnen@live.no>
2019-03-22 09:09:18 +0100

    Add html View

--- app.py
+++ app.py
@@ -1,66 +1,67 @@
-from flask import Flask
-from flask_admin import Admin, BaseView, expose
+from flask import Flask, render_template, redirect, url_for, request
+from flask_admin import Admin
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from flask_admin.contrib.sqla import ModelView
-from Models.Department import Department
-<<<<<<< HEAD
-from Models.Equipment import Equipment
-from Models.Employee import Employee
-from Models.Receipt import Receipt
-from Models.Transaction import Transaction
-=======
 from Models.Employee import Employee
 
->>>>>>> flaskAdminViews
 
 app = Flask(__name__)
+
 database = create_engine('postgres://postgres:admin@localhost:5432/mydatabase')
 database.connect()
+
+admin = Admin(app)
+
+app.config['SECRET_KEY'] = 'lameKey'
 
 Session = sessionmaker(database)
 session = Session()
 
-<<<<<<< HEAD
-# Comment in if you want it to create the database for you
-# Base.metadata.create_all(database)
-
-test_department = Department(country="Norway", unit="Oslo")
-session.add(test_department)
-session.commit()
-
-test_employee = Employee(id="E23", name="Endre", department_id=test_department.id)
-session.add(test_employee)
-session.commit()
-
-test_receipt = Receipt(id="newRec", supplement="UAB", year=2019)
-session.add(test_receipt)
-session.commit()
-
-test_equipment = Equipment(model="Apple Pay", price=1992, description="make me", note="hehe")
-session.add(test_equipment)
-session.commit()
-
-test_transaction = Transaction(equipment_id=test_equipment.id, employee_id=test_employee.id)
-session.add(test_transaction)
-session.commit()
-
-myList = session.query(Department)
-
-for a in myList:
-    print(type(a))
-
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('Views/index.html')
 
 
-=======
->>>>>>> flaskAdminViews
-admin = Admin(app)
-admin.add_view(ModelView(Department, session))
-admin.add_view(ModelView())
+@app.route('/employee')
+def employee():
+    data = session.query(Employee)
+    return render_template('Views/Employee/index.html', data=data)
+
+
+@app.route('/employee/delete/<int:postID>', methods=['POST'])
+def delete_employee(postID):
+    print(postID)
+    return redirect(url_for('employee'))
+
+
+@app.route('/employee/edit/<int:empID>')
+def edit_employee(empID):
+    empID = str(empID)
+    person = session.query(Employee).get(empID)
+    print(person.start_date)
+    return render_template('Views/Employee/edit.html', data=person)
+
+
+@app.route('/equipment')
+def equipment():
+    return render_template('Views/Equipment/index.html')
+
+
+@app.route('/receipt')
+def receipt():
+    return render_template('Views/Receipt/index.html')
+
+
+@app.route('/transaction')
+def transaction():
+    return render_template('Views/Transaction/index.html')
+
+
+@app.route('/department')
+def department():
+    return render_template('Views/Department/index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
