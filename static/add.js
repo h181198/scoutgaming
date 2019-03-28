9f2365a4ffadaf596764c63a6f3152c35fc96672
elisabethmarie <150494@stud.hvl.no>
2019-03-28 12:03:26 +0100

    Fix date-field on add function

--- static/add.js
+++ static/add.js
@@ -6,30 +6,35 @@ var table = document.getElementById("table");
 
 var row = table.insertRow(-1);
 
+
 for(let i = 0; i < 6; i++){
 
+
           var x = document.createElement("INPUT");
-          x.setAttribute("type", "text");
+
+        if(table.rows[0].cells[i].classList.contains("date")) {
+        x.setAttribute("type", "date");
+        } else {
+        x.setAttribute("type", "text");
+        }
+
           x.setAttribute("size", 8)
           cell = row.insertCell(i);
           cell.appendChild(x)
 
-
-
-
         }
-  let confirmButton = document.createElement("INPUT");
+        let confirmButton = document.createElement("INPUT");
         confirmButton.setAttribute("type", "submit");
         confirmButton.setAttribute("value", "Confirm");
         confirmButton.setAttribute("class", "btn btn-info");
-         row.insertCell(6).appendChild(confirmButton);
+        row.insertCell(6).appendChild(confirmButton);
 
 
-          let cancelButton = document.createElement("INPUT");
+        let cancelButton = document.createElement("INPUT");
         cancelButton.setAttribute("type", "submit");
         cancelButton.setAttribute("value", "Cancel");
         cancelButton.setAttribute("class", "btn btn-danger");
-         row.insertCell(7).appendChild(cancelButton);
+        row.insertCell(7).appendChild(cancelButton);
 
          cancelButton.addEventListener("click", function () {
          table.deleteRow(-1);
