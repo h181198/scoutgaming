be1f9621c0184593834b087216c49e5f0cb624c0
h181198 <h181198@stud.hvl.no>
2019-04-23 14:10:16 +0200

    Implement functionality in receipt page > implement add, delete and edit

--- Controllers/ReceiptController.py
+++ Controllers/ReceiptController.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request, redirect, url_for
 from jinja2 import TemplateNotFound
 from Controllers import session
+from Services.ReceiptService import ReceiptService
+from Services.DeleteService import DeleteService
+from Helpers.HelpMethods import create_data, create_single_id
 
 receipt_page = Blueprint('receipt', __name__)
 
@@ -8,6 +11,37 @@ receipt_page = Blueprint('receipt', __name__)
 @receipt_page.route('/receipt')
 def receipt():
     try:
-        return render_template('Views/Receipt/index.html')
+        data = ReceiptService.get_all_receipts(session)
+        return render_template('Views/Receipt/index.html', data=data)
     except TemplateNotFound:
         abort(404)
+
+
+@receipt_page.route('/receipt/add', methods=['POST'])
+def add_receipt():
+    try:
+        ReceiptService.add_receipt(session=session, supplement=request.form['supplement'],
+                                   year=int(request.form['year']))
+        return redirect(url_for('receipt.receipt'))
+    except TemplateNotFound:
+        abort(404)
+    return ""
+
+
+@receipt_page.route('/receipt/update', methods=['POST'])
+def update_receipt():
+    try:
+        data = create_data(str(request.data.decode('utf8')))
+        ReceiptService.update_receipt(session, data[0], data[1], int(data[2]))
+        return ReceiptService.get_receipt_json(session, data[0])
+    except TemplateNotFound:
+        abort(404)
+
+
+@receipt_page.route('/receipt/delete', methods=['POST'])
+def delete_receipt():
+    try:
+        DeleteService.delete_receipt(session=session, rec_id=create_single_id(str(request.data))[0])
+    except TemplateNotFound:
+        abort(404)
+    return ""
